22996e872b5c653011b7724e946fc4a597e86a4c
George Gkenios <83859471+gkenios@users.noreply.github.com>
2026-05-12 16:31:31 +0200

    Add header

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,23 @@
-import { Education, Experience, Footer, Intro, Skills } from './components';
+import { Education, Experience, Footer, Header, Intro, ScrollProgress, Skills } from './components';
 import { EDUCATION_ENTRIES, EXPERIENCE_ENTRIES, SKILLS_CATEGORIES } from './data';
 
 export default function App() {
   return (
     <div className="min-h-screen bg-bg text-foreground font-sans selection:bg-accent selection:text-black">
-      <Intro />
-      <Skills categories={SKILLS_CATEGORIES} />
-      <Experience experienceEntries={EXPERIENCE_ENTRIES} />
-      <Education educationEntries={EDUCATION_ENTRIES} />
+      <ScrollProgress />
+      <Header />
+      <div id="intro" className="scroll-mt-20">
+        <Intro />
+      </div>
+      <div id="skills" className="scroll-mt-20">
+        <Skills categories={SKILLS_CATEGORIES} />
+      </div>
+      <div id="experience" className="scroll-mt-20">
+        <Experience experienceEntries={EXPERIENCE_ENTRIES} />
+      </div>
+      <div id="education" className="scroll-mt-20">
+        <Education educationEntries={EDUCATION_ENTRIES} />
+      </div>
       <Footer />
     </div>
   );
